f50a79e06a1867b4c8ba992d00234494697498ce
Eddie Alex <edmlounge@gmail.com>
2018-04-30 12:37:17 -0400

    Adjusted media queries

--- src/PortfolioList.js
+++ src/PortfolioList.js
@@ -16,17 +16,24 @@ const clockAppDescription1 =
   "This is a weather app I built with React JS. The app can instantly detect where there is daylight and adjusts the styles accordingly."
 const clockAppDescription2 =
   "When a city is clicked, more information including the current weather, news, and photos load. This incorporates two APIs."
+const casinoDescription1 =
+  "This is an online Bitcoin-powered casino concept I built with Rails. It uses WebSockets and animations to create instant, entertaining multiplayer gameplay."
+const casinoDescription2 =
+  "Users can view their online friends list and create private poker rooms that support up to eight players. A private Bitcoin wallet is initialized for every user on account creation, with the private key stored encrypted in our SQL database."
 
 export class PortfolioList extends React.Component {
   render() {
     return(
       <div className="PortfolioList">
-        <PortfolioItem link="http://www.edmlounge.com" id="edm" description1={edmDescription1} description2={edmDescription2} title="EDM Lounge" images={{
-          './pretty-lights-nyc.jpg': 'Pretty Lights at Roseland Ballroom',
+        <PortfolioItem link="http://www.edmlounge.com" id="edm" description1={edmDescription1} description2={edmDescription2} title="EDM Lounge" images={
+          {'./pretty-lights-nyc.jpg': 'Pretty Lights at Roseland Ballroom',
           './pretty-lights-nyc-2.jpg': 'Pretty Lights at Roseland Ballroom',
           './beetroots-edc.jpg': 'The Bloody Beetroots at EDC Las Vegas'
           }} />
         <PortfolioItem title="React JS World Clock" id="clock" link="http://react-world-clock-app.com" description1={clockAppDescription1} description2={clockAppDescription2} images={{'./world-clock-app-1.png': 'World Clock', './world-clock-app-2.png': 'World Clock'}} />
+        <PortfolioItem title="Rails Casino" link="" id="casino" description1={casinoDescription1} description2={casinoDescription2} images={
+          {'./casino1.png': 'Blackjack Gameplay',
+           './casino2.png': 'Poker Gameplay'}} />
       </div>
       )
   }
--- src/TechGrid.js
+++ src/TechGrid.js
@@ -8,34 +8,42 @@ export class TechGrid extends React.Component {
       <div className="TechGrid">
         <div className="TechIcon">
           <img src={require('./ruby.png')} alt="Ruby"/>
+          <br/>
           Ruby
         </div>
         <div className="TechIcon">
           <img src={require('./rails.png')} alt="Rails"/>
+          <br/>
           Rails
         </div>
         <div className="TechIcon">
           <img src={require('./react.png')} alt="React Js"/>
+          <br/>
           React JS
         </div>
         <div className="TechIcon">
           <img src={require('./node-js.png')} alt="Node JS"/>
+          <br/>
           Node JS
         </div>
         <div className="TechIcon">
           <img src={require('./photoshop.png')} alt="Photoshop"/>
+          <br/>
           Adobe Photoshop
         </div>
         <div className="TechIcon">
           <img src={require('./html5.png')} alt="HTML5"/>
+          <br/>
           HTML5
         </div>
         <div className="TechIcon">
           <img src={require('./css.png')} alt="CSS3"/>
+          <br/>
           CSS3
         </div>
         <div className="TechIcon">
           <img src={require('./sql.png')} alt="SQL"/>
+          <br/>
           SQL
         </div>
       </div>
