3ef84d470c4f35c3bb1e5e2ad2ad88f79b26708f
Eddie Alex <edmlounge@gmail.com>
2018-05-02 13:44:05 -0400

    Fixed message modal

--- src/Message.js
+++ src/Message.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './Message.css';
 
 export class Message extends React.Component {
+  componentDidMount() {
+    this.nameInput.focus();
+  }
+
   render() {
     return(
       <div className="Message-Wrapper">
         <form method="POST" action="https://formspree.io/edmlounge@gmail.com">
-          <input type="email" name="email" placeholder="Your email" />
+          <input ref={(input) => { this.nameInput = input; }} type="email" name="email" placeholder="Your email" />
           <textarea name="message" placeholder="Your message"></textarea>
           <button type="submit">Send</button>
         </form>
--- src/TopMenu.js
+++ src/TopMenu.js
@@ -24,6 +24,11 @@ export class TopMenu extends React.Component {
   }
 
   msgClick() {
+    if (this.state.bioClicked) {
+      this.setState({bioClicked: false});
+    } else if (this.state.twitterClicked) {
+        this.setState({twitterClicked: false})
+    }
     this.state.msgClicked ? this.setState({msgClicked: false}) : this.setState({msgClicked: true})
   }
 
@@ -32,11 +37,11 @@ export class TopMenu extends React.Component {
   }
 
   render() {
-    if (this.state.twitterClicked === true) {
+    if (this.state.twitterClicked) {
       return(
         <div className="Top-Menu">
           <ul className="dropdown">
-            <MenuButton name="Message Me" icon={<FontAwesome.FaEnvelope />}/>
+            <span onClick={this.msgClick.bind(this)}><MenuButton name="Message Me" icon={<FontAwesome.FaEnvelope />}/></span>
             <span onClick={this.twitterClick.bind(this)}><MenuButton name="Twitter" icon={<FontAwesome.FaTwitter />} /></span>
             <MenuButton name="SoundCloud" icon={<FontAwesome.FaSoundcloud/>}/>
             <a target="_blank" href="http://www.github.com/phant0mnyc"><MenuButton name="Github" icon={<FontAwesome.FaGithub />}/></a>
@@ -47,60 +52,44 @@ export class TopMenu extends React.Component {
         </div>
       )
     }
-    else if (this.state.bioClicked === true && this.state.msgClicked === true) {
+    else if (this.state.bioClicked) {
       return(
         <div className="Top-Menu">
           <ul className="dropdown">
-           <MenuButton name="Message Me" icon={<FontAwesome.FaEnvelope />}/>
+            <span onClick={this.msgClick.bind(this)}><MenuButton name="Message Me" icon={<FontAwesome.FaEnvelope />}/></span>
             <span onClick={this.twitterClick.bind(this)}><MenuButton name="Twitter" icon={<FontAwesome.FaTwitter />} /></span>
             <MenuButton name="SoundCloud" icon={<FontAwesome.FaSoundcloud/>}/>
             <a target="_blank" href="http://www.github.com/phant0mnyc"><MenuButton name="Github" icon={<FontAwesome.FaGithub />}/></a>
             <a target="_blank" href="https://www.linkedin.com/in/eafancher/"><MenuButton name="LinkedIn" icon={<FontAwesome.FaLinkedinSquare />} /></a>
             <span onClick={this.bioClick.bind(this)}><MenuButton name="Bio" icon={<FontAwesome.FaAngleDown/>} onClick={this.bioClick.bind(this)}/></span>
           </ul>
-          <Message />
           <div className="Bio-Wrap">
             <Bio modal="web"/>
             <Bio modal="resume"/>
           </div>
         </div>
         )
-    } else if (this.state.bioClicked === true && this.state.msgClicked === false) {
+    } else if (this.state.msgClicked) {
       return(
+        <div>
         <div className="Top-Menu">
           <ul className="dropdown">
-           <MenuButton name="Message Me" icon={<FontAwesome.FaEnvelope />}/>
+            <span onClick={this.msgClick.bind(this)}><MenuButton name="Message Me" icon={<FontAwesome.FaEnvelope />}/></span>
             <span onClick={this.twitterClick.bind(this)}><MenuButton name="Twitter" icon={<FontAwesome.FaTwitter />} /></span>
             <MenuButton name="SoundCloud" icon={<FontAwesome.FaSoundcloud/>}/>
             <a target="_blank" href="http://www.github.com/phant0mnyc"><MenuButton name="Github" icon={<FontAwesome.FaGithub />}/></a>
             <a target="_blank" href="https://www.linkedin.com/in/eafancher/"><MenuButton name="LinkedIn" icon={<FontAwesome.FaLinkedinSquare />} /></a>
             <span onClick={this.bioClick.bind(this)}><MenuButton name="Bio" icon={<FontAwesome.FaAngleDown/>} onClick={this.bioClick.bind(this)}/></span>
           </ul>
-          <div className="Bio-Wrap">
-            <Bio modal="web"/>
-            <Bio modal="resume"/>
-          </div>
         </div>
-      );
-    } else if (this.state.msgClicked === true) {
-      return(
-        <div className="Top-Menu">
-          <ul className="dropdown">
-            <MenuButton name="Message Me" icon={<FontAwesome.FaEnvelope />}/>
-            <span onClick={this.twitterClick.bind(this)}><MenuButton name="Twitter" icon={<FontAwesome.FaTwitter />} /></span>
-            <MenuButton name="SoundCloud" icon={<FontAwesome.FaSoundcloud/>}/>
-            <a target="_blank" href="http://www.github.com/phant0mnyc"><MenuButton name="Github" icon={<FontAwesome.FaGithub />}/></a>
-            <a target="_blank" href="https://www.linkedin.com/in/eafancher/"><MenuButton name="LinkedIn" icon={<FontAwesome.FaLinkedinSquare />} /></a>
-            <span onClick={this.bioClick.bind(this)}><MenuButton name="Bio" icon={<FontAwesome.FaAngleDown/>} onClick={this.bioClick.bind(this)}/></span>
-          </ul>
-          <Message />
+        <Message />
         </div>
       )
     } else {
       return(
       <div className="Top-Menu">
         <ul className="dropdown">
-            <MenuButton name="Message Me" icon={<FontAwesome.FaEnvelope />}/>
+            <span onClick={this.msgClick.bind(this)}><MenuButton name="Message Me" icon={<FontAwesome.FaEnvelope />}/></span>
             <span onClick={this.twitterClick.bind(this)}><MenuButton name="Twitter" icon={<FontAwesome.FaTwitter />} /></span>
             <MenuButton name="SoundCloud" icon={<FontAwesome.FaSoundcloud/>}/>
             <a target="_blank" href="http://www.github.com/phant0mnyc"><MenuButton name="Github" icon={<FontAwesome.FaGithub />}/></a>
