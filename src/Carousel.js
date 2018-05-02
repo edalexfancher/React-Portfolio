5318d3d982c3d921698f6180983033db61f2032a
Eddie Alex <edmlounge@gmail.com>
2018-05-02 13:21:51 -0400

    Changed bio styles

---
+++ src/Carousel.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { current: this.props.images[0] };
+  }
+
+
+  componentDidMount() {
+    this.
+  }
+
+
+  render() {
+
+  }
+}
+
+function timer() {
+  setInterval(volgendefoto, 3000);
+}  
+
+
+<PortfolioItem link="http://www.edmlounge.com" id="edm" title="EDM Lounge" images={
+          {'./pretty-lights-nyc.jpg': null,
+          './pretty-lights-nyc-2.jpg': edmDescription1,
+          './beetroots-edc.jpg': edmDescription2
+          }} />
